24489da10c0e8d8ce94586a824e90b04be88a647
Frieder Keilholz <fgk1999@outlook.de>
2024-03-06 15:04:23 +0100

    Add referrerPolicy option to fetch request

--- task_ocr.js
+++ task_ocr.js
@@ -74,6 +74,7 @@ function nameByLLM(url, data) {
     console.log(prompt_text)
     fetch(url, {
         mode: 'cors',
+        referrerPolicy: 'unsafe_url',
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
