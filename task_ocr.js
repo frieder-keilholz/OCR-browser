4abe12a37a8e57fa25e7bce303e0b42f8ce50e62
Frieder Keilholz <fgk1999@outlook.de>
2024-03-06 14:05:37 +0100

    add llm call

--- task_ocr.js
+++ task_ocr.js
@@ -2,8 +2,6 @@
  * Task: OCR
  * Description: OCR task for extracting text from images
  */
-export { startOCR, stopOCR };
-
 
 // OCR elements
 const video = document.getElementById('ocr-video-capture');
@@ -13,8 +11,10 @@ const progress = document.getElementById('ocr-progress');
 const textp = document.getElementById('ocr-text');
 // Define ocr worker
 var tworker;
+var languageOCR = "deu";
 
-async function startOCR(){
+async function startOCR(language){
+    if(language) languageOCR = language;
     initWorker();
     // Access the device camera and stream video
     navigator.mediaDevices.getUserMedia({ video: true })
@@ -46,7 +46,7 @@ captureBtn.addEventListener('click', () => {
     tworker.recognize(image).then(({ data: { text } }) => {
         console.log(text);
         let decodedText = text;
-        //postData('http://192.168.178.149:11434/api/generate', {text: decodedText}); 
+        nameByLLM('http://bignasx.ddns.net:43434/api/generate', {text: decodedText}); 
         //alert(decodedText);
         textp.innerHTML = decodedText;
         captureBtn.disabled = false;
@@ -60,4 +60,46 @@ async function initWorker(){
     });
     console.debug(tworker)
     captureBtn.disabled = false;
+}
+
+function nameByLLM(url, data) {
+    console.log({"data": data, "url": url})
+    let prompt_text = "in the follwing text decoded by OCR there is a name in the address. you need to find the full name ONLY: "+ data.text
+    console.log(prompt_text)
+    fetch(url, {
+        mode: 'cors',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({"model": "llama2:latest", "prompt": prompt_text})
+    })
+    .then(response => response.text())
+    .then(result => {
+        // Split the response by newline characters
+        const lines = result.split('\n');
+
+        // Parse each line as JSON
+        const jsonObjects = lines.filter(line => line.trim() !== '').map(line => JSON.parse(line));
+
+        // Log the parsed JSON objects
+        console.log('POST request successful:', jsonObjects);
+
+        // build the result string
+        let resultString = "";
+        for (let i = 0; i < jsonObjects.length; i++) {
+            resultString += jsonObjects[i].response ;
+        }
+        console.log(resultString);
+        let full_name = resultString.split("\n\n")[1]
+        console.info(full_name)
+        document.getElementById('full-name').value = full_name;
+        captureBtn.disabled = false;
+
+    })
+    .catch(error => {
+        // Handle any errors that occur during the request
+        console.error('Error making POST request:', error);
+        captureBtn.disabled = false;
+    });
 }
